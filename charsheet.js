0e01063db5bd1dbf888583eb3956ce9d2f396052
Matthias Haan <dr.ace@gmx.net>
2017-07-08 09:34:16 +0200

    Added new focus from 0.11

--- charsheet.js
+++ charsheet.js
@@ -453,6 +453,15 @@ var staticData = {
 	        ]
         },
         {
+            "name": "Star Captain",
+            "type": "normal",
+            "details": [
+	        "You have a tremendous natural talent for ship combat, and can make any starship you captain a significantly more fearsome opponent. You must take the captain's role during a fight as described on page XX of the Ship Combat rules in order to benefit from this focus.",
+	        "<b>Level 1</b>: Gain Lead as a bonus skill. Your ship gains 2 extra Command Points at the start of each turn.",
+	        "<b>Level 2</b>: A ship you captain gains bonus hit points equal to 20% of its maximum at the start of each combat. Damage is taken from these bonus points first, and they vanish at the end of the fight and do not require repairs to replenish before the next. In addition, once per engagement, you may resolve a Crisis as an Instant action by explaining how your leadership resolves the problem."
+            ]
+        },
+        {
             "name": "Starfarer",
 	        "type": "normal",
 	        "details": [
